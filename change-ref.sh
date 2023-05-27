0ec49ea1b262382b91d42503d8c13e1cf938c184
fspiers <fredspiers@gmail.com>
2023-05-28 00:29:11 +0200

    fixed substitution

--- change-ref.sh
+++ change-ref.sh
@@ -13,13 +13,15 @@ PROJECT_REF=$1
 
 for file in $DIR_WORKFLOWS
 do
-    for line in $(yq e '.jobs[].steps[].uses | select(.=="'"$ACTIONS_ORGA"'*@*")' $file)
+    for line in $(yq e '.jobs[].steps[].uses | select(.) | select(.=="'"$ACTIONS_ORGA"'*@*")' $file)
+    # | select(.) to avoid null
     do
         ACTION=$(echo "$line" | awk -F/ '{print $NF}')
         REF=$(echo "$line" | awk -F@ '{print $NF}')
         if [[ "$REF" != "$PROJECT_REF" ]]; then
             echo -e "${CYAN}[ INFO ] > File ${YELLOW}$file${CYAN} and action ${PURPLE}$ACTION${CYAN} is currently set to ref ${RED}$REF${CYAN} and will be changed to ${GREEN}$PROJECT_REF${BLANK}"
-            yq e -i '.jobs[].steps[].uses |= select(.=="'"$line"'") |= sub("'"@$REF"'", "'"@$PROJECT_REF"'")' $file
+            yq e -i '.jobs[].steps[] |= select(.uses).uses |= select(.=="'"$line"'") |= sub("'"@$REF"'", "'"@$PROJECT_REF"'")' $file
+            # "|= select(.uses).uses" prevent insertiopn of element not wanted like "null"
         else
             echo -e "${PURPLE}[ CANCEL ] > File ${YELLOW}$file${PURPLE} and action ${RED}$ACTION${PURPLE} is already set to ${RED}$PROJECT_REF${PURPLE} ref${BLANK}"
         fi
