323cbab80787e03e9da5847167eab7e271d34a40
fspiers <fredspiers@gmail.com>
2023-05-28 23:34:38 +0200

    auto tag bump2

--- change-ref.sh
+++ change-ref.sh
@@ -11,26 +11,19 @@ ACTIONS_ORGA=ixxeL-actions
 DIR_WORKFLOWS="./.github/workflows/*.yaml"
 PROJECT_REF=$1
 
-# for file in $DIR_WORKFLOWS
-# do
-#     for line in $(yq e '.jobs[].steps[].uses | select(.) | select(.=="'"$ACTIONS_ORGA"'*@*")' $file)
-#     # | select(.) to avoid null
-#     do
-#         ACTION=$(echo "$line" | awk -F/ '{print $NF}')
-#         REF=$(echo "$line" | awk -F@ '{print $NF}')
-#         if [[ "$REF" != "$PROJECT_REF" ]]; then
-#             echo -e "${CYAN}[ INFO ] > File ${YELLOW}$file${CYAN} and action ${PURPLE}$ACTION${CYAN} is currently set to ref ${RED}$REF${CYAN} and will be changed to ${GREEN}$PROJECT_REF${BLANK}"
-#             yq e -i '.jobs[].steps[] |= select(.uses).uses |= select(.=="'"$line"'") |= sub("'"@$REF"'", "'"@$PROJECT_REF"'")' $file
-#             # "|= select(.uses).uses" prevent insertiopn of element not wanted like "null"
-#         else
-#             echo -e "${PURPLE}[ CANCEL ] > File ${YELLOW}$file${PURPLE} and action ${RED}$ACTION${PURPLE} is already set to ${RED}$PROJECT_REF${PURPLE} ref${BLANK}"
-#         fi
-#     done
-# done
-PATTERN='v[0-9].[0-9].[0-9]'
-if [[ -n $(git describe --tags --match $PATTERN --abbrev=0) ]]; then
-    git describe --tags --match $PATTERN --abbrev=0
-    echo "not empty"
-else
-    echo "empty"
-fi
+for file in $DIR_WORKFLOWS
+do
+    for line in $(yq e '.jobs[].steps[].uses | select(.) | select(.=="'"$ACTIONS_ORGA"'*@*")' $file)
+    # | select(.) to avoid null
+    do
+        ACTION=$(echo "$line" | awk -F/ '{print $NF}')
+        REF=$(echo "$line" | awk -F@ '{print $NF}')
+        if [[ "$REF" != "$PROJECT_REF" ]]; then
+            echo -e "${CYAN}[ INFO ] > File ${YELLOW}$file${CYAN} and action ${PURPLE}$ACTION${CYAN} is currently set to ref ${RED}$REF${CYAN} and will be changed to ${GREEN}$PROJECT_REF${BLANK}"
+            yq e -i '.jobs[].steps[] |= select(.uses).uses |= select(.=="'"$line"'") |= sub("'"@$REF"'", "'"@$PROJECT_REF"'")' $file
+            # "|= select(.uses).uses" prevent insertiopn of element not wanted like "null"
+        else
+            echo -e "${PURPLE}[ CANCEL ] > File ${YELLOW}$file${PURPLE} and action ${RED}$ACTION${PURPLE} is already set to ${RED}$PROJECT_REF${PURPLE} ref${BLANK}"
+        fi
+    done
+done
