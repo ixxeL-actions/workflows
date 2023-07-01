fcacbc70ef5d7811f2bd0e01cd21c806dda1342e
ixxel2097 <fredspiers@gmail.com>
2023-07-01 12:48:07 +0200

    revert to main

--- change-ref.sh
+++ change-ref.sh
@@ -10,20 +10,35 @@ BLANK="\033[0m"
 ACTIONS_ORGA=ixxeL-actions
 DIR_WORKFLOWS="./.github/workflows/*.yaml"
 PROJECT_REF=$1
-
+ 
 for file in $DIR_WORKFLOWS
 do
     for line in $(yq e '.jobs[].steps[].uses | select(.) | select(.=="'"$ACTIONS_ORGA"'*@*")' $file)
     # | select(.) to avoid null
     do
-        ACTION=$(echo "$line" | awk -F/ '{print $NF}')
+        ACTION=$(echo "$line" | awk -F@ '{print $1}' | awk -F/ '{print $NF}')
         REF=$(echo "$line" | awk -F@ '{print $NF}')
         if [[ "$REF" != "$PROJECT_REF" ]]; then
-            echo -e "${CYAN}[ INFO ] > File ${YELLOW}$file${CYAN} and action ${PURPLE}$ACTION${CYAN} is currently set to ref ${RED}$REF${CYAN} and will be changed to ${GREEN}$PROJECT_REF${BLANK}"
+            echo -e "${CYAN}[ INFO ] > File ${YELLOW}$file${CYAN} and ${BLUE}action${CYAN} ${PURPLE}$ACTION${CYAN} is currently set to ref ${RED}$REF${CYAN} and will be changed to ${GREEN}$PROJECT_REF${BLANK}"
             yq e -i '.jobs[].steps[] |= select(.uses).uses |= select(.=="'"$line"'") |= sub("'"@$REF"'", "'"@$PROJECT_REF"'")' $file
             # "|= select(.uses).uses" prevent insertiopn of element not wanted like "null"
         else
-            echo -e "${PURPLE}[ CANCEL ] > File ${YELLOW}$file${PURPLE} and action ${RED}$ACTION${PURPLE} is already set to ${RED}$PROJECT_REF${PURPLE} ref${BLANK}"
+            echo -e "${PURPLE}[ CANCEL ] > File ${YELLOW}$file${PURPLE} and ${BLUE}action${CYAN} ${RED}$ACTION${PURPLE} is already set to ${RED}$PROJECT_REF${PURPLE} ref${BLANK}"
         fi
+    done
+
+    for key in $(yq e '.jobs | select(.) | keys' $file)
+    do
+        for line in $(yq e '.jobs."'"$key"'".uses | select(.) | select(.=="'"$ACTIONS_ORGA"'*@*")' $file)
+        do
+            ACTION=$(echo "$line" | awk -F@ '{print $1}' | awk -F/ '{print $NF}')
+            REF=$(echo "$line" | awk -F@ '{print $NF}')
+            if [[ "$REF" != "$PROJECT_REF" ]]; then
+                echo -e "${CYAN}[ INFO ] > File ${YELLOW}$file${CYAN} and ${BLUE}workflow${CYAN} ${PURPLE}$ACTION${CYAN} is currently set to ref ${RED}$REF${CYAN} and will be changed to ${GREEN}$PROJECT_REF${BLANK}"
+                yq e -i '.jobs."'"$key"'" |= select(.uses).uses |= select(.=="'"$line"'") |= sub("'"@$REF"'", "'"@$PROJECT_REF"'")' $file
+            else
+                echo -e "${PURPLE}[ CANCEL ] > File ${YELLOW}$file${PURPLE} and ${BLUE}workflow${CYAN} ${RED}$ACTION${PURPLE} is already set to ${RED}$PROJECT_REF${PURPLE} ref${BLANK}"
+            fi
+        done
     done
 done
