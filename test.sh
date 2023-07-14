0f7a17a8c2fb68cfa2f220b6e1f3c9f3ffa12594
ixxeL2097 <fredspiers@gmail.com>
2023-07-15 01:16:03 +0200

    fix: using non containerized job 2

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+BLANK="\033[0m"
+
+
+VERSION=test-2.0.0
+inputs=test
+
+VERSION=${VERSION//${inputs}-/ }
+
+echo $VERSION
